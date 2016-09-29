580d31d82a08919fdc1f482fcf273b3f26614e6c
davojta <damarah@gmail.com>
2016-09-29 13:54:45 +0300

    fix paths

--- js/init.js
+++ js/init.js
@@ -3,18 +3,19 @@
 	html5up.net | @n33co
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
+var prefix = '/video';
 
 (function($) {
 
 	skel.init({
 		reset: 'full',
 		breakpoints: {
-			global:		{ range: '*', href: '/css/style.css', containers: 1400, grid: { gutters: 50 } },
-			wide:		{ range: '-1680', href: '/css/style-wide.css', containers: 1200, grid: { gutters: 40 } },
-			normal:		{ range: '-1280', href: '/css/style-normal.css', containers: 960, viewport: { scalable: false } },
-			narrow:		{ range: '-980', href: '/css/style-narrow.css', containers: '95%', grid: { gutters: 30 } },
-			narrower:	{ range: '-840', href: '/css/style-narrower.css', grid: { collapse: 1 } },
-			mobile:		{ range: '-736', href: '/css/style-mobile.css', containers: '100%', grid: { gutters: 15, collapse: 2 } }
+			global:		{ range: '*', href: prefix + '/css/style.css', containers: 1400, grid: { gutters: 50 } },
+			wide:		{ range: '-1680', href: prefix + '/css/style-wide.css', containers: 1200, grid: { gutters: 40 } },
+			normal:		{ range: '-1280', href: prefix + '/css/style-normal.css', containers: 960, viewport: { scalable: false } },
+			narrow:		{ range: '-980', href: prefix + '/css/style-narrow.css', containers: '95%', grid: { gutters: 30 } },
+			narrower:	{ range: '-840', href: prefix + '/css/style-narrower.css', grid: { collapse: 1 } },
+			mobile:		{ range: '-736', href: prefix + '/css/style-mobile.css', containers: '100%', grid: { gutters: 15, collapse: 2 } }
 		},
 		plugins: {
 			layers: {
