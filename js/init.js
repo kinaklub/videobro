7b47b68755996232ddf57364a16994697598ea61
Dzianis Sheka <damarah@gmail.com>
2016-10-31 15:07:17 +0300

    Update init.js

--- js/init.js
+++ js/init.js
@@ -3,7 +3,7 @@
 	html5up.net | @n33co
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
-var prefix = '/video';
+var prefix = '/videobro';
 
 (function($) {
 
